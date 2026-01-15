e76b35862df6a6fac4d193b51a24ea096f81faf6
Mauro Soliz <mauro.soliz@believesol.com>
2026-01-15 12:15:11 -0400

    Movie Card completo

--- src/components/movies/MovieGrid.tsx
+++ src/components/movies/MovieGrid.tsx
@@ -7,6 +7,46 @@ interface Props {
     movies: Movie[]
     loading: boolean
 }
+interface ContentProps {
+    children: React.ReactNode
+    key: any
+
+}
+const GridContent = ({ children, key }: ContentProps) => {
+    return (
+        <Grid
+            key={key}
+            size={{ xs: 12, sm: 6, md: 3, lg: 3 }}
+            sx={{ display: 'flex', justifyContent: 'center' }}
+        >
+            {children}
+        </Grid>
+    )
+}
+
+const SkeletonCard = () => {
+    return (
+        Array.from({ length: 8 }).map((_, index) => (
+            <GridContent key={index}>
+                <Skeleton
+                    variant='rectangular'
+                    animation='wave'
+                    sx={{ width: 320, height: 480 }}
+                />
+            </GridContent>
+        ))
+    )
+}
+
+const MovieCards = ({ movies }: { movies: Movie[] }) => {
+    return (
+        movies.map(movie => (
+            <GridContent key={movie.id}>
+                <MovieCard movie={movie} />
+            </GridContent>
+        ))
+    )
+}
 
 const MovieGrid = ({ movies, loading }: Props) => {
     return (
@@ -16,15 +56,7 @@ const MovieGrid = ({ movies, loading }: Props) => {
             padding={3}
             sx={{ width: '100%', marginInline: 0 }}
         >
-            {movies.map(movie => (
-                <Grid
-                    key={movie.id}
-                    size={{ xs: 12, sm: 6, md: 3, lg: 3 }}
-                    sx={{ display: 'flex', justifyContent: 'center' }}
-                >
-                    <MovieCard movie={movie} />
-                </Grid>
-            ))}
+            {loading ? SkeletonCard() : <MovieCards movies={movies} />}
         </Grid>
     )
 }
