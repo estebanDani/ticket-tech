e7630da9f125769610568fd56bdc79721f0dc27d
Mauro Soliz <mauro.soliz@believesol.com>
2026-02-03 12:39:50 -0400

    comenzando

---
+++ src/components/admin/TheaterForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, TextField, Typography } from '@mui/material'
+
+const TheaterForm = () => {
+    return (
+        <Container maxWidth={false}>
+            <Typography variant='h4'>Crear Sala</Typography>
+
+        </Container>
+    )
+}
+
+export default TheaterForm
