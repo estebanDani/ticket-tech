e885937adfd02c0d3cd5c0afa6320bcf5f411d65
Mauro Soliz <mauro.soliz@believesol.com>
2026-02-10 14:10:42 -0400

    Lint errors solved

--- src/app/admin/theaters/page.tsx
+++ src/app/admin/theaters/page.tsx
@@ -2,20 +2,10 @@
 import { Box, Container, Button } from '@mui/material'
 import PageHeader from '@/components/common/PageHeader'
 import TheaterTable from './TheaterTable'
-import { Theater } from '@/types'
 import { useTheaters } from '@/hooks'
 
 export default function TheatersPage() {
   const { theaters } = useTheaters();
-
-  const handleOpenCreate = () => {
-  }
-
-  const handleEdit = (theater: Theater) => {
-  }
-
-  const handleDelete = (theater: Theater) => {
-  }
 
   return (
     <Container maxWidth={false} sx={{ mt: 4 }}>
@@ -25,14 +15,14 @@ export default function TheatersPage() {
           description="Administra tus salas"
           icon="🎪"
         />
-        <Button variant="contained" onClick={handleOpenCreate}>
+        <Button variant="contained">
           Nueva Sala
         </Button>
       </Box>
       <TheaterTable
         theaters={theaters}
-        onEdit={handleEdit}
-        onDelete={handleDelete}
+        onEdit={() => { }}
+        onDelete={() => { }}
       />
     </Container>
   )
