fd0c462039bf33731928c391040b55ffb8a4b844
Shadow <henry.aspeti@believesol.com>
2026-02-12 19:21:38 -0400

    Solucion deerrores finales

--- src/components/admin/showtime/ShowtimeDelete.tsx
+++ src/components/admin/showtime/ShowtimeDelete.tsx
@@ -2,20 +2,19 @@ import { Button, DialogActions, DialogContent, DialogTitle, Typography } from "@
 
 interface FormDeleteProps {
   handleDelete: () => void;
-  movieName: string;
-  date: string;
+  title: string;
   submit: boolean;
 }
 
-export const ShowtimeDelete = ({ movieName, date, handleDelete, submit }: FormDeleteProps) => {
+export const ShowtimeDelete = ({ title, handleDelete, submit }: FormDeleteProps) => {
   return (
     <>
       <DialogTitle>Eliminar Función</DialogTitle>
       <DialogContent>
         <Typography>
-          ¿Estás seguro de eliminar la función de <strong>{movieName}</strong> programada para el día <strong>{date}</strong>?
+          ¿Estás seguro de eliminar la función de <strong>{title}</strong>?
         </Typography>
-        <Typography variant="caption" color="error">
+        <Typography variant="caption" color="error" sx={{ display: 'block', mt: 1 }}>
           Esta acción no se puede deshacer.
         </Typography>
       </DialogContent>
