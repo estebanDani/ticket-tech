9c1162585d694f6139b24929995b82392f3916a7
Mauro Soliz <mauro.soliz@believesol.com>
2026-02-20 16:22:10 -0400

    fix: Al hacer logout se eliminan las cookies

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -8,32 +8,32 @@ interface AuthContextType {
     loading: boolean;
 
     login: (email: string, password: string) => Promise<void>;
-    register:(user: CreateUserDto, password: string) => Promise<void>;
+    register: (user: CreateUserDto, password: string) => Promise<void>;
     logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    
+
     const [user, setUserState] = useState<User | null>(null);
     const [loading, setLoadingState] = useState<boolean>(true);
 
     const getCurrentUser = async () => {
 
         try {
-            const currentUser = await  AuthService.getCurrentUser()
+            const currentUser = await AuthService.getCurrentUser()
             setUserState(currentUser);
-        } catch  {
+        } catch {
             setUserState(null);
-        }finally {
+        } finally {
             setLoadingState(false);
         }
     }
 
-    useEffect( () => {
+    useEffect(() => {
         getCurrentUser();
-    },[])
+    }, [])
 
     const login = async (email: string, password: string) => {
         setLoadingState(true);
@@ -41,12 +41,12 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
             const loggedUser = await AuthService.login(email, password);
             setUserState(loggedUser);
             document.cookie = "authToken=true; path=/;";
-            document.cookie = "role="+loggedUser.role+"; path=/;";
+            document.cookie = "role=" + loggedUser.role + "; path=/;";
         } finally {
             setLoadingState(false);
         }
     }
-    
+
     const register = async (user: CreateUserDto, password: string) => {
         setLoadingState(true);
         try {
@@ -56,16 +56,18 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
             setLoadingState(false);
         }
     }
-    
+
     const logout = async () => {
         setLoadingState(true);
         try {
             await AuthService.logout();
             setUserState(null);
+            document.cookie = "authToken=; path=/; max-age=0;";
+            document.cookie = "role=; path=/; max-age=0;";
         } finally {
             setLoadingState(false);
         }
-    }   
+    }
 
     return (
         <AuthContext.Provider value={{ user, loading, login, register, logout }}>
@@ -75,9 +77,9 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
 };
 
 export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error('auseAuth must be used within AuthProvider');
-  }
-  return context;
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error('auseAuth must be used within AuthProvider');
+    }
+    return context;
 }
